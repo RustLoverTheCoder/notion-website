fee7c09e7ea63412c5883424436b70bc944a3fcd
MaXiang <71162666+Toursslivers@users.noreply.github.com>
2023-08-28 17:43:57 +0800

    feat: add main nav

--- src/components/MainNav/index.tsx
+++ src/components/MainNav/index.tsx
@@ -13,41 +13,84 @@ import React from "react";
 import { cn } from "@/lib/utils";
 
 export const MainNav = qwikify$(() => {
+  const downLoadList = [
+    {
+      title: "iOS",
+      href: "/download/ios",
+      description:
+        "A modal dialog that interrupts the user with important content and expects a response.",
+    },
+    {
+      title: "Android",
+      href: "/download/android",
+      description:
+        "A modal dialog that interrupts the user with important content and expects a response.",
+    },
+    {
+      title: "Mac",
+      href: "/download/mac",
+      description:
+        "A modal dialog that interrupts the user with important content and expects a response.",
+    },
+    {
+      title: "Windows",
+      href: "/download/windows",
+      description:
+        "A modal dialog that interrupts the user with important content and expects a response.",
+    },
+    {
+      title: "Linux",
+      href: "/download/linux",
+      description:
+        "A modal dialog that interrupts the user with important content and expects a response.",
+    },
+    {
+      title: "Web-Clipper",
+      href: "/download/web-clipper",
+      description:
+        "A modal dialog that interrupts the user with important content and expects a response.",
+    },
+  ];
+
+  const SolutionsList = [
+    {
+      title: "Enterprise",
+      href: "/enterprise",
+      description:
+        "A modal dialog that interrupts the user with important content and expects a response.",
+    },
+    {
+      title: "Small business",
+      href: "/teams",
+      description:
+        "A modal dialog that interrupts the user with important content and expects a response.",
+    },
+    {
+      title: "Personal",
+      href: "/personal",
+      description:
+        "A modal dialog that interrupts the user with important content and expects a response.",
+    },
+  ];
+
   const list = [
     {
-      title: "Alert Dialog",
+      title: "Blog",
       href: "/docs/primitives/alert-dialog",
       description:
         "A modal dialog that interrupts the user with important content and expects a response.",
     },
     {
-      title: "Hover Card",
+      title: "Guides & tutorials",
       href: "/docs/primitives/hover-card",
       description:
         "For sighted users to preview content available behind a link.",
     },
     {
-      title: "Progress",
+      title: "API Docs",
       href: "/docs/primitives/progress",
       description:
         "Displays an indicator showing the completion progress of a task, typically displayed as a progress bar.",
-    },
-    {
-      title: "Scroll-area",
-      href: "/docs/primitives/scroll-area",
-      description: "Visually or semantically separates content.",
-    },
-    {
-      title: "Tabs",
-      href: "/docs/primitives/tabs",
-      description:
-        "A set of layered sections of content—known as tab panels—that are displayed one at a time.",
-    },
-    {
-      title: "Tooltip",
-      href: "/docs/primitives/tooltip",
-      description:
-        "A popup that displays information related to an element when the element receives keyboard focus or the mouse hovers over it.",
     },
   ];
   return (
@@ -55,7 +98,7 @@ export const MainNav = qwikify$(() => {
       <NavigationMenu>
         <NavigationMenuList>
           <NavigationMenuItem>
-            <NavigationMenuTrigger>Getting started</NavigationMenuTrigger>
+            <NavigationMenuTrigger>Product</NavigationMenuTrigger>
             <NavigationMenuContent>
               <ul className="grid gap-3 p-6 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
                 <li className="row-span-3">
@@ -74,22 +117,54 @@ export const MainNav = qwikify$(() => {
                     </a>
                   </NavigationMenuLink>
                 </li>
-                <ListItem href="/docs" title="Introduction">
+                <ListItem href="/docs" title="Wikis">
                   Re-usable components built using Radix UI and Tailwind CSS.
                 </ListItem>
-                <ListItem href="/docs/installation" title="Installation">
+                <ListItem href="/docs/installation" title="Projects">
                   How to install dependencies and structure your app.
                 </ListItem>
-                <ListItem href="/docs/primitives/typography" title="Typography">
+                <ListItem href="/docs/primitives/typography" title="Docs">
                   Styles for headings, paragraphs, lists...etc
                 </ListItem>
               </ul>
             </NavigationMenuContent>
           </NavigationMenuItem>
           <NavigationMenuItem>
-            <NavigationMenuTrigger>Components</NavigationMenuTrigger>
+            <NavigationMenuTrigger>Download</NavigationMenuTrigger>
             <NavigationMenuContent>
               <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
+                {downLoadList.map((component) => (
+                  <ListItem
+                    key={component.title}
+                    title={component.title}
+                    href={component.href}
+                  >
+                    {component.description}
+                  </ListItem>
+                ))}
+              </ul>
+            </NavigationMenuContent>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <NavigationMenuTrigger>Solutions</NavigationMenuTrigger>
+            <NavigationMenuContent>
+              <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-1 lg:w-[600px] ">
+                {SolutionsList.map((component) => (
+                  <ListItem
+                    key={component.title}
+                    title={component.title}
+                    href={component.href}
+                  >
+                    {component.description}
+                  </ListItem>
+                ))}
+              </ul>
+            </NavigationMenuContent>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <NavigationMenuTrigger>Resources</NavigationMenuTrigger>
+            <NavigationMenuContent>
+              <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-1 lg:w-[600px] ">
                 {list.map((component) => (
                   <ListItem
                     key={component.title}
@@ -103,9 +178,9 @@ export const MainNav = qwikify$(() => {
             </NavigationMenuContent>
           </NavigationMenuItem>
           <NavigationMenuItem>
-            <a href="/docs">
+            <a href="/pricing">
               <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                Documentation
+                pricing
               </NavigationMenuLink>
             </a>
           </NavigationMenuItem>
