14d8765f4e932dd89dc379b9967255138d924c84
MaXiang <71162666+Toursslivers@users.noreply.github.com>
2023-08-31 14:30:55 +0800

    feat: add mobile

--- src/routes/desktop/index.tsx
+++ src/routes/desktop/index.tsx
@@ -1,17 +1,24 @@
 import { component$ } from "@builder.io/qwik";
+import Idea from "/images/idea-launch.svg";
+import { ButtonQwik as Button } from "@/components/ui/button";
 export default component$(() => {
   return (
     <>
-      <section class="space-y-6 pb-8 pt-6 md:pb-12 md:pt-10 lg:py-32">
+      <section class="space-y-6 pb-8 pt-6 md:pb-12 md:pt-10 lg:py-16">
         <div class="container flex max-w-[64rem] flex-col items-center gap-4 text-center">
-            
-          <h1 class="font-heading text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-medium">
-            An example app built using Next.js 13 server components.
+          <img src={Idea} alt="image" width={360} height={360} />
+          <hr class="h-5" />
+          <h1 class="font-heading text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-semibold">
+            Notion for Mac, Windows, and Linux 
           </h1>
-          <p class="max-w-[42rem] leading-normal text-muted-foreground sm:text-xl sm:leading-8">
-            I&apos;m building a web app with Next.js 13 and open sourcing
-            everything. Follow along as we figure this out together.
-          </p>
+          <h2 class="max-w-[42rem] leading-normal text-muted-foreground sm:text-xl sm:leading-8">
+            Work without distraction on your own or with your team.
+          </h2>
+          <div class="space-x-4">
+            <Button size="lg">Download for Mac</Button>
+            <Button size="lg">Download for Windows</Button>
+            <Button size="lg">Download for Linux</Button>
+          </div>
         </div>
       </section>
     </>
--- src/routes/mobile/index.tsx
+++ src/routes/mobile/index.tsx
@@ -1,4 +1,25 @@
 import { component$ } from "@builder.io/qwik";
+import design from "/images/graphic-design.svg";
+import { ButtonQwik as Button } from "@/components/ui/button";
 export default component$(() => {
-  return <>mobile</>;
+  return (
+    <>
+      <section class="space-y-6 pb-8 pt-6 md:pb-12 md:pt-10 lg:py-16">
+        <div class="container flex max-w-[64rem] flex-col items-center gap-4 text-center">
+          <img src={design} alt="image" width={360} height={360} />
+          <hr class="h-5" />
+          <h1 class="font-heading text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-semibold">
+            Notion for iOS and Android
+          </h1>
+          <h2 class="max-w-[42rem] leading-normal text-muted-foreground sm:text-xl sm:leading-8">
+            Work without distraction on your own or with your team.
+          </h2>
+          <div class="space-x-4">
+            <Button size="lg">Download for iOS</Button>
+            <Button size="lg">Download for Android</Button>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 });
